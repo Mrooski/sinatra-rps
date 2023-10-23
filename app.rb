5d93ed28745999302145e5af250e0f46296b9c58
Mrooski <107450558+Mrooski@users.noreply.github.com>
2023-10-23 15:54:30 +0000

    Rock Works

--- app.rb
+++ app.rb
@@ -2,8 +2,29 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+erb(:main)
+end
+
+
+get("/rock") do
+  @player_move = "We played rock!"
+  @rand_move = rand(1..3)
+  case @rand_move
+  when 1
+    @move_output = "They played rock!"
+    @result = "We tied!"
+  when 2
+    @move_output = "They played scissors!"
+    @result = "We won!"
+  when 3
+    @move_output = "They played papper!"
+    @result = "We lost!"
+  end
+  erb(:rock)
+end
+
+get("/paper") do
+end
+
+get("/scissors") do
 end
