e10f99c62c88e248ae7256e2072cb333891f7a6f
Mrooski <107450558+Mrooski@users.noreply.github.com>
2023-10-23 16:04:53 +0000

    100% on the rake grade. No rules in the "/" domain

--- app.rb
+++ app.rb
@@ -17,14 +17,42 @@ get("/rock") do
     @move_output = "They played scissors!"
     @result = "We won!"
   when 3
-    @move_output = "They played papper!"
+    @move_output = "They played paper!"
     @result = "We lost!"
   end
   erb(:rock)
 end
 
 get("/paper") do
+  @player_move = "We played paper!"
+  @rand_move = rand(1..3)
+  case @rand_move
+  when 1
+    @move_output = "They played rock!"
+    @result = "We won!"
+  when 2
+    @move_output = "They played scissors!"
+    @result = "We lost!"
+  when 3
+    @move_output = "They played paper!"
+    @result = "We tied!"
+  end
+  erb(:rock)
 end
 
 get("/scissors") do
+  @player_move = "We played scissors!"
+  @rand_move = rand(1..3)
+  case @rand_move
+  when 1
+    @move_output = "They played rock!"
+    @result = "We lost!"
+  when 2
+    @move_output = "They played scissors!"
+    @result = "We tied!"
+  when 3
+    @move_output = "They played paper!"
+    @result = "We won!"
+  end
+  erb(:rock)
 end
